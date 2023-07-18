67017d59954144c64c4db89e7b6d95dcd6252fd1
Seungji Baek <lisa2009343123456@gmail.com>
2023-07-18 14:51:33 +0900

    feat :: 지도 추가

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.example.demo.dto.RequestDto;
 import com.example.demo.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,15 @@ public class HomeController {
     @Autowired
     RecordService recordService;
 
+    @Value("${api.key}")
+    private String getKey;
+
     @GetMapping(value = "/")
     public String home(Model model, RequestDto requestDto) {
         model.addAttribute("requestDto",requestDto);
         model.addAttribute("recordList", recordService.getRecordList());
         model.addAttribute("workList", recordService.getWorksList());
+        model.addAttribute("APPKEY",getKey);
         return "home/index";
     }
 
