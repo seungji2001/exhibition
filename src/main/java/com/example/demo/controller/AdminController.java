99546f2b11e106a37c6d94b4cdb844bff3a027e9
Seungji Baek <lisa2009343123456@gmail.com>
2023-08-14 10:08:28 +0900

    admin front

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,20 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
+import com.example.demo.dto.exhibitionDto.ExhibitionRequestDto;
 import com.example.demo.dto.memberDto.MemberRequestDto;
 import com.example.demo.dto.workDto.WorkRequestDto;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.service.ExhibitionService;
+import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RequiredArgsConstructor
 @Controller
 public class AdminController {
-    //세로운 멤버 등록페이지로 넘어가기
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    ExhibitionService exhibitionService;
+
     @GetMapping(value = "/admin")
-    public String registrationMemebr(){
-        return "admin/NiceAdmin/users-profile.html";
+    public String registrationMemebr(Model model
+            , ExhibitionRequestDto.registrationExhibition registrationExhibition
+    ){
+
+        return "admin/NiceAdmin/adminMember/admin-profile";
     }
 }
