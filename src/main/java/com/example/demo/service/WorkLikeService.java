69f65f3c4bb1efd4769f4b9b7233bf280a578322
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-10-18 23:12:06 +0900

    [like-400] 수정사항 적용

--- src/main/java/com/example/demo/service/WorkLikeService.java
+++ src/main/java/com/example/demo/service/WorkLikeService.java
@@ -9,6 +9,7 @@ import com.example.demo.event.LikeCreatedEvent;
 import com.example.demo.repository.WorkLikeRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.WorkRepository;
+import jakarta.persistence.NoResultException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
@@ -32,17 +33,18 @@ public class WorkLikeService {
     @Autowired
     ApplicationEventPublisher eventPublisher;
 
-    public Long createLike(CreateLikeDTO createLikeDTO) {
+    @Transactional
+    public Long createLike(LikeRequestDto.createLike createLike) {
         //null의 경우에만 workLike 객체 생성 가능하도록, member나 work가 없는 경우 예외처리 바로 해주세요
-        Member member = memberRepository.findById(createLikeDTO.getMemberId()).orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 없습니다."));
-        Work work = workRepository.findById(createLikeDTO.getWorkId()).orElseThrow(() -> new IllegalArgumentException("해당하는 작품이 없습니다."));
+        Member member = memberRepository.findById(createLike.getMemberId()).orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 없습니다."));
+        Work work = workRepository.findById(createLike.getWorkId()).orElseThrow(() -> new IllegalArgumentException("해당하는 작품이 없습니다."));
 
         WorkLike like = WorkLike.builder()
                 .memberId(member)
                 .workId(work)
                 .build();
 
-        workLikeRepository.save(like);
+        work.updateLikeCount();
 
         //new 생성자 되도록 쓰지 말아주세요 -> builder 패턴 지향
         LikeCreatedEvent event = new LikeCreatedEvent(this, like);
@@ -51,24 +53,29 @@ public class WorkLikeService {
         return workLikeRepository.save(like).getId();
     }
 
+    @Transactional
     public String deleteLike(Long member_id, Long work_id){
-
         Member member = memberRepository.findById(member_id).orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 없습니다."));
         Work work = workRepository.findById(work_id).orElseThrow(() -> new IllegalArgumentException("해당하는 작품이 없습니다."));
 
-        WorkLike like = workLikeRepository.findByMemberIdAndWorkId(member, work).orElseThrow(() -> new IllegalArgumentException("해당하는 좋아요가 없습니다."));
+        WorkLike like = workLikeRepository.findByMemberIdAndWorkId(member.getId(), work.getId()).orElseThrow(() -> new IllegalArgumentException("해당하는 좋아요가 없습니다."));
 
-        workLikeRepository.deleteByMemberIdAndWorkId(member, work);
+        workLikeRepository.deleteById(like.getId());
+        work.deleteLikeCount();
 
         return "success";
     }
 
+    @Transactional
     public Long countLike(Long work_id){
 
         Work work = workRepository.findById(work_id).orElseThrow(() -> new IllegalArgumentException("해당하는 작품이 없습니다."));
-        Long like = workLikeRepository.countByWorkId(work);
-
-        return like;
-
+        Long like = workLikeRepository.countByWorkId(work.getId());
+        if(work.getLikeCount() == like){
+            return like;
+        }
+        else{
+            throw new IllegalArgumentException("");
+        }
     }
 }
