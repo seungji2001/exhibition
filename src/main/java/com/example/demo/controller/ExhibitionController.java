3a99a224979ed772a69952ca64bab3a8b81946e3
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-10-18 17:19:53 +0900

    [exhibition-1] 와이어 프레임 바인딩

--- src/main/java/com/example/demo/controller/ExhibitionController.java
+++ src/main/java/com/example/demo/controller/ExhibitionController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,15 +18,11 @@ public class ExhibitionController {
     @Autowired
     ExhibitionService exhibitionService;
 
-    @GetMapping(value = "/exhibition")
-    public String getExhi(){
-        return "/exhibition/index1";
-    }
-
     //exhibition 가져오기
     @GetMapping(value = "/exhibition/{exhibition_id}")
-    public ResponseEntity<ExhibitionResponseDto.getExhibition> getExhibition(@PathVariable("exhibition_id") Long exhibition_id){
-        return ResponseEntity.ok().body(exhibitionService.getExhibition(exhibition_id));
+    public String getExhibition(Model model, @PathVariable("exhibition_id") Long exhibition_id){
+        model.addAttribute("getExhibition",exhibitionService.getExhibition(exhibition_id));
+        return "/exhibition/index1";
     }
 
     //exhibition 등록
