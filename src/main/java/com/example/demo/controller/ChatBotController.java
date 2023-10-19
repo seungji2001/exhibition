fc538752e129a59a56b60ff96ab54442a511f427
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-10-19 22:48:42 +0900

    [exhibition-500] WikiQA api 기능 개발

--- src/main/java/com/example/demo/controller/ChatBotController.java
+++ src/main/java/com/example/demo/controller/ChatBotController.java
@@ -1,12 +1,27 @@
 package com.example.demo.controller;
 
+import com.example.demo.Component.OpenAPIManager;
+import com.example.demo.dto.ChatBotDto.ChatBotRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.io.UnsupportedEncodingException;
+
+@RestController
 public class ChatBotController {
+
+    @Autowired
+    OpenAPIManager openAPIManager;
 
     @Value("${wiki.api.key}")
     private String wikiKey;
 
+    @PostMapping(value = "/chatbot")
+    public void getWikiQA(@RequestBody ChatBotRequestDto.RequestQuestion chatBotRequestDto) throws UnsupportedEncodingException, JsonProcessingException {
+        openAPIManager.getWikiQA("http://aiopen.etri.re.kr:8000/WikiQA", wikiKey,"irqa",chatBotRequestDto);
+    }
 }
