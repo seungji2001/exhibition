fec4147937302d1bce48a893513f0a7941c5bdd4
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-10-25 17:46:12 +0900

    [exhibition-3] 템플릿 추가

---
+++ src/main/java/com/example/demo/service/ChatBotService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.ChatBotDto.ChatBotRequestDto;
+import com.example.demo.dto.ChatBotDto.ChatBotResponseDto;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatBotService {
+
+//    @Transactional
+//    public ChatBotResponseDto.ResponseAnswer getAnswer(ChatBotRequestDto.RequestQuestion question){
+//        //질문에 해당되는 답변 만들어 return 하기
+//
+//    }
+}
