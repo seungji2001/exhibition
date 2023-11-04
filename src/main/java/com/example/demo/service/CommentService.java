7f3851b8751471afdb10fdef07dfb56937aeabc1
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-11-04 13:46:54 +0900

    [exhibition-602] put /comment 댓글 수정 기능 추가

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.Member;
 import com.example.demo.domain.ReplyComment;
 import com.example.demo.domain.Work;
 import com.example.demo.dto.commentDto.CommentRequestDto;
+import com.example.demo.dto.commentDto.CommentResponseDto;
 import com.example.demo.repository.CommentRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.ReplyCommentRepository;
@@ -35,6 +36,29 @@ public class CommentService {
                 .build();
         return commentRepository.save(comment).getId();
     }
+
+    @Transactional
+    public CommentResponseDto.GetCommentResponse updateComment(CommentRequestDto.UpdateComment updateComment){
+        Member member = memberRepository.findById(updateComment.getMember_id()).orElseThrow();
+        Comment comment = commentRepository.findById(updateComment.getComment_id()).orElseThrow();
+
+        //작성한 멤버만 수정이 가능 하다
+        if(!comment.getMember().equals(member))
+            throw new IllegalArgumentException();
+
+        //변경 사항이 없으면 수정 안한다
+        if(comment.getContent().equals(updateComment.getContent()))
+            throw new IllegalArgumentException("변경사항이 없습니다.");
+
+        comment.updateComment(updateComment.getContent());
+        commentRepository.save(comment);
+
+        return CommentResponseDto.GetCommentResponse.builder()
+                .comment_id(updateComment.getComment_id())
+                .content(updateComment.getContent())
+                .build();
+    }
+
     public Long  saveReplyComment(Long comment_id, CommentRequestDto.RegistrationReplyComment registrationReplyComment){
         Member member = memberRepository.findById(registrationReplyComment.getMember_id()).orElseThrow();
         Comment comment = commentRepository.findById(comment_id).orElseThrow();
