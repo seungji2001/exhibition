55d241a7b6b1aa66c6878b07121d8d4dbb46606f
Seungji Baek <seungjibaek@Seungjis-MacBook-Pro.local>
2023-11-05 04:03:11 +0900

    [exhibition-602] service 생성

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Comment;
+import com.example.demo.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByWork(Work work);
 }
