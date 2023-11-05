f0db9e386713314159cd0f0361ef9103525302b0
jihye0623 <wisdxx@gmail.com>
2023-11-05 15:07:32 +0900

    대댓글 수정기능 추가

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -103,4 +103,53 @@ public class CommentService {
                 .build();
         return replyCommentRepository.save(replyComment).getId();
     }
+
+    public List<CommentResponseDto.GetReplyCommentsResponse> getReplyComment(Long comment_id){
+        Comment comment = commentRepository.findById(comment_id).orElseThrow();
+        List<ReplyComment> replyComments = replyCommentRepository.findAllByComment(comment);
+
+        List<CommentResponseDto.GetReplyCommentsResponse> getReplyCommentsResponses = new ArrayList<>();
+
+        if(replyComments.isEmpty()){
+            return getReplyCommentsResponses;
+        }
+        getReplyCommentsResponses.addAll(
+                replyComments.stream()
+                        .map(replyComment -> {
+                            return CommentResponseDto.GetReplyCommentsResponse.builder()
+                                    .reply_comment_id(replyComment.getId())
+                                    .content(replyComment.getContent())
+                                    .writer(replyComment.getMember().getName())
+                                    .insertDate(replyComment.getInsertDate())
+                                    .modifiedDate(replyComment.getModifiedDate())
+                                    .modified(replyComment.getModified())
+                                    .build();
+                        })
+                        .toList()
+        );
+        return getReplyCommentsResponses;
+    }
+
+
+    public CommentResponseDto.GetReplyCommentResponse updateReplyComment(CommentRequestDto.UpdateReplyComment updateReplyComment){
+        Member member = memberRepository.findById(updateReplyComment.getMember_id()).orElseThrow();
+        ReplyComment replyComment = replyCommentRepository.findById(updateReplyComment.getReply_comment_id()).orElseThrow();
+
+        //작성한 멤버만 수정이 가능 하다
+        if(!replyComment.getMember().equals(member))
+            throw new IllegalArgumentException();
+
+        //변경 사항이 없으면 수정 안한다
+        if(replyComment.getContent().equals(updateReplyComment.getContent()))
+            throw new IllegalArgumentException("변경사항이 없습니다.");
+
+        replyComment.updateReplyComment(updateReplyComment.getContent());
+        replyCommentRepository.save(replyComment);
+
+        return CommentResponseDto.GetReplyCommentResponse.builder()
+                .reply_comment_id(updateReplyComment.getReply_comment_id())
+                .writer(member.getName())
+                .content(updateReplyComment.getContent())
+                .build();
+    }
 }
