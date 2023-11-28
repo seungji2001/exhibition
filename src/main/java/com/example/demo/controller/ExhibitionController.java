86d21bd4f89f79ceaea97bbf256cc099a3b8a67e
jihye0623 <wisdxx@gmail.com>
2023-11-29 00:08:22 +0900

    index4 바인딩

--- src/main/java/com/example/demo/controller/ExhibitionController.java
+++ src/main/java/com/example/demo/controller/ExhibitionController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.dto.exhibitionDto.ExhibitionRequestDto;
 import com.example.demo.dto.exhibitionDto.ExhibitionResponseDto;
 import com.example.demo.service.ExhibitionService;
+import com.example.demo.service.WorkService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +18,17 @@ public class ExhibitionController {
 
     @Autowired
     ExhibitionService exhibitionService;
+    @Autowired
+    WorkService workService;
 
     //exhibition 가져오기
     @GetMapping(value = "/exhibition/{exhibition_id}")
     public String getExhibition(Model model, @PathVariable("exhibition_id") Long exhibition_id){
         model.addAttribute("getExhibition",exhibitionService.getExhibition(exhibition_id));
-        return "/exhibition/index1";
+
+        model.addAttribute("workList", workService.getAllWork(exhibition_id));
+
+        return "/exhibition/index4";
     }
 
     //exhibition 등록
